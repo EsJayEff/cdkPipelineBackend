f25de1693ba96330c7c57f0d2b367013de10f9b3
Shiraz Javed Farooq <114878673+EsJayEff@users.noreply.github.com>
2023-09-13 21:08:42 +0500

    first commit

---
+++ lib/backend_deployment-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
+
+export class BackendDeploymentStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const productTable = new ddb.Table(this, 'Table', {
+      tableName:"TestTable",
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+  }
+}
